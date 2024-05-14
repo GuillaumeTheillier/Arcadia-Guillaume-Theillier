8d6430b159f7309bfaf6dde62a5f275abefbe4e3
Soahren <guillaume.thl12@gmail.com>
2024-05-14 20:06:12 +0200

    Putting into function open and close code for frame create and update account form

--- script/crudScript.js
+++ script/crudScript.js
@@ -1,15 +1,33 @@
-const btnOpen = document.getElementById('btn-crud-add');
-const btnClose = document.getElementById('btn-crud-close')
-const cadre = document.getElementById('cadre');
+//const btnOpen = document.getElementById('btn-crud-add');
+//const btnClose = document.getElementById('btn-crud-close')
 
 
-cadre.style.display = 'none';
+function openCreateAccount() {
+    const frame = document.getElementById('createAccountFrame');
+    frame.style.display = 'block';
+}
 
-btnOpen.addEventListener('click', () => {
-    console.log('test');
-    cadre.style.display = 'block';
-});
+function closeCreateAccount() {
+    const frame = document.getElementById('createAccountFrame');
+    frame.style.display = 'none';
+}
 
-btnClose.addEventListener('click', () => {
-    cadre.style.display = 'none';
-});
+function openUpdateAccount() {
+    const frame = document.getElementById('updateAccountFrame');
+    frame.style.display = 'block';
+}
+
+function closeUpdateAccount() {
+    const frame = document.getElementById('updateAccountFrame');
+    frame.style.display = 'none';
+}
+
+function showPassword() {
+    const passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    }
+    else {
+        passwordInput.type = 'password';
+    }
+}
