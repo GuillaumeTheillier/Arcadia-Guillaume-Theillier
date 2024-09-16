cae3f3a329c22e7b7b890ccae9fde1e521a163b5
Soahren <guillaume.thl12@gmail.com>
2024-09-17 01:13:15 +0200

    Check if animalId is set and put it in cookie. And check if blank is not used in addFoodConsumption function.

--- src/controllers/foodConsumptionController.php
+++ src/controllers/foodConsumptionController.php
@@ -8,27 +8,77 @@ function foodConsumptionRepository()
 
 function foodConsumptionForm()
 {
+    if (isset($_POST['animalId'])) {
+        $animalId = $_POST['animalId'];
+    } else if (isset($_COOKIE['ANIMAL_ID'])) {
+        $animalId =  $_COOKIE['ANIMAL_ID'];
+    } else {
+        setcookie(
+            'ADD_FOOD_ERROR',
+            'Une erreur est survenue.',
+            [
+                'expires' => time() + 2,
+                'httponly' => true,
+                'secure' => true
+            ]
+        );
+        //Redirect to the animal list page
+        redirectToUrl('index.php?action=animalList');
+    }
+    //Save animal id for few minutes if they are an error 
+    setcookie(
+        'ANIMAL_ID',
+        $animalId,
+        [
+            'expires' => time() + 300,
+            'httponly' => true,
+            'secure' => true
+        ]
+    );
     require('templates/foodConsumptionForm.php');
 }
 
 function addFoodConsumption()
 {
     if (isset($_POST['foodDate']) && isset($_POST['foodType']) && isset($_POST['foodQuantity'])) {
-        $date = $_POST['foodDate'];
-        $foodType = htmlspecialchars($_POST['foodType']);
-        $quantity = htmlspecialchars($_POST['foodQuantity']);
         $username = $_SESSION['LOGGED_USER'];
         $animalId = $_POST['animalId'];
-        //var_dump($date);
-        foodConsumptionRepository()->addConsumption($date, $foodType, $quantity, $username, $animalId);
-        //Redirect to the animal page
-        redirectToUrl('index.php?action=habitatsList');
+        if (!ctype_space($_POST['foodDate']) && !ctype_space($_POST['foodType']) && !ctype_space($_POST['foodQuantity'])) {
+            $date = $_POST['foodDate'];
+            $foodType = htmlspecialchars($_POST['foodType']);
+            $quantity = htmlspecialchars($_POST['foodQuantity']);
+
+            $success = foodConsumptionRepository()->addConsumption($date, $foodType, $quantity, $username, $animalId);
+            setcookie(
+                'ADD_FOOD_SUCCESS',
+                $success,
+                [
+                    'expires' => time() + 2,
+                    'httponly' => true,
+                    'secure' => true
+                ]
+            );
+            //Redirect to the animal list page
+            redirectToUrl('index.php?action=animalList');
+        } else {
+            setcookie(
+                'ADD_FOOD_ERROR',
+                'Tous les champs doivent être remplis.',
+                [
+                    'expires' => time() + 2,
+                    'httponly' => true,
+                    'secure' => true
+                ]
+            );
+            //Redirect to the add food page
+            redirectToUrl('index.php?action=foodConsumptionForm');
+        }
     } else {
         setcookie(
             'ADD_FOOD_ERROR',
-            'Tous les champs n\'ont pas été saisis',
+            'Tous les champs n\'ont pas été saisis.',
             [
-                'expires' => time() + 1,
+                'expires' => time() + 2,
                 'httponly' => true,
                 'secure' => true
             ]
