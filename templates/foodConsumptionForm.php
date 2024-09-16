b6de908aa592c9ecf2e086717ace595d7885c7fe
Soahren <guillaume.thl12@gmail.com>
2024-09-17 01:09:14 +0200

    Display error on adding food consumption and return animal id on specific animal.

--- templates/foodConsumptionForm.php
+++ templates/foodConsumptionForm.php
@@ -5,11 +5,18 @@ ob_start();
 
 <main>
     <h1 class="page-title">Ajouter une consommation de nourriture</h1>
+    <!-- Alert -->
+    <div class="alert-container">
+        <?php if (isset($_COOKIE['ADD_FOOD_ERROR'])) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $_COOKIE['ADD_FOOD_ERROR']; ?>
+                <button type='button' class="btn-close ms-auto" data-bs-dismiss='alert'></button>
+            </div>
+        <?php endif; ?>
+    </div>
     <form action="index.php?action=addFoodConsumption" method="post">
         <div class="input-container">
             <label for="food-date" class="label-input-form">Date</label>
-            <!--<input type="date" name="foodDate" id="food-date" class="input-form" required>
-            <input type="time" name="foodDate" id="food-date" class="input-form" required>-->
             <input type="datetime-local" name="foodDate" id="food-date" class="input-form" required>
         </div>
         <div class="input-container">
@@ -20,7 +27,7 @@ ob_start();
             <label for="food-quantity" class="label-input-form">Quantité</label>
             <input type="text" name="foodQuantity" id="food-quantity" class="input-form" required>
         </div>
-        <input type="text" name="animalId" id="animal-id" hidden value="1">
+        <input type="text" name="animalId" id="animal-id" hidden value="<?php echo $animalId; ?>">
         <button type="submit">Ajouter</button>
     </form>
 </main>
