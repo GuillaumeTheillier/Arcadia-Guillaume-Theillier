0a1a36394ad5af71000a1db72d84d08c6a9261e9
Soahren <guillaume.thl12@gmail.com>
2024-09-18 23:23:46 +0200

    frame for add habitat comment

---
+++ templates/crudForm/addHabitatComment.php
@@ -0,0 +1,24 @@
+<div class="create-form-container">
+    <div class="crud-frame">
+        <button type='button' class="btn-close btn-close-frame"></button>
+
+        <h4 class="crud-frame-title">Ajouter un commentaire à un habitat</h4>
+
+        <div class="alert-container">
+            <?php if (isset($_COOKIE['ADD_HABITAT_COMMENT_ERROR'])) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $_COOKIE['ADD_HABITAT_COMMENT_ERROR']; ?>
+                    <button type='button' class="btn-close ms-auto" data-bs-dismiss='alert'></button>
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <form action="index.php?action=addAHabitatComment" method="post">
+            <div class="input-container">
+                <label for="add-habitat-comment" class="label-input-form"></label>
+                <input type="text" class="input-form" name="addHabitatComment" id="add-habitat-comment" maxlength="255" required>
+            </div>
+            <button type="submit" name="habitatId" value="<?php echo $habitat['id'] ?>">Confirmer</button>
+        </form>
+    </div>
+</div>
