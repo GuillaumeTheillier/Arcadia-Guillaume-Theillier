05cf7f8d4aadb8241625e980ae3a90ac6c4a1c90
Soahren <guillaume.thl12@gmail.com>
2024-09-24 18:40:50 +0200

    update frameScript to open frame on a page who they are multiple button

--- script/frameScript.js
+++ script/frameScript.js
@@ -1,14 +1,26 @@
-let btnOpen = document.querySelector('.btn-open-frame');
+let btnOpen = document.querySelectorAll('.btn-open-frame');
 let btnClose = document.querySelector('.btn-close-frame');
 let frame = document.querySelector('.crud-frame');
 let body = document.querySelector('body');
 //console.log(frame);
 
+function openFrameWithMultipleButton() {
+    btnOpen.forEach(element => {
+        let btnSubmit = document.querySelector('.submitFrameBtn');
+        element.addEventListener('click', () => {
+            btnSubmit.value = element.dataset.animalId;
+            //console.log(element.dataset.animalId);
+            frame.classList.add('visible');
+            body.style.overflow = 'hidden';
+        });
+    });
+}
+
 /**
  * Open frame on button click and disable the scrollbar for the main page.
  */
 function openFrame() {
-    btnOpen.addEventListener('click', () => {
+    btnOpen[0].addEventListener('click', () => {
         frame.classList.add('visible');
         body.style.overflow = 'hidden';
     });
@@ -24,5 +36,12 @@ function closeFrame() {
     });
 }
 
-openFrame();
-closeFrame();
+
+if (btnOpen.length > 1) {
+    openFrameWithMultipleButton()
+    closeFrame();
+} else if (btnOpen.length === 1) {
+    openFrame();
+    closeFrame();
+}
+
