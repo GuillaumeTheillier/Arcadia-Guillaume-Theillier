c264fbc7cbc481640573feec81be7eb4c3ace2cc
Soahren <guillaume.thl12@gmail.com>
2024-09-26 00:17:01 +0200

    update to get all report

--- src/model/veterinarianReport.php
+++ src/model/veterinarianReport.php
@@ -43,20 +43,101 @@ class VeterinarianReportRepository
         return $statement->execute();
     }
 
-    function getReport(int $animalId)
+    /**
+     *  Get report associates at one animals.
+     * 
+     * @param int $animalId animal's id 
+     * @return array All report of the animal are given in param.
+     */
+    function getReport(int $animalId): array
     {
-        $consumptionList = [];
+        $reportList = array();
         $statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity
                                                                 FROM food_consumption
                                                                 WHERE animal_id = ?
                                                                 ORDER BY date DESC');
         $statement->execute([$animalId]);
-        while ($consumption = $statement->fetch(pdo::FETCH_ASSOC)) {
-            $date = new DateTime($consumption['date']);
-            $consumption['date'] = date_format($date, "d/m/Y H:i:s");
-
-            $consumptionList[] = $consumption;
+        while ($report = $statement->fetch(pdo::FETCH_ASSOC)) {
+            $date = new DateTime($report['date']);
+            $report['date'] = date_format($date, "d/m/Y H:i:s");
+            $reportList[] = $report;
         }
-        return $consumptionList;
+        return $reportList;
+    }
+
+    /**
+     *  Get all veterinarian reports.
+     * 
+     * @return array All animal's reports.
+     */
+    function getAllReport(string $sort = null, int|null $animalId = null, string|null $date = null)
+    {
+        $reportList = array();
+
+        if ($sort === null || $sort === 'dateAsc') {
+            $statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY date ASC');
+        } elseif ($sort === 'dateDesc') {
+            $statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY date DESC');
+        } elseif ($sort === 'animalNameAsc') {
+            $statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY animalName ASC');
+        } elseif ($sort === 'animalNameDesc') {
+            $statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY animalName DESC');
+        }
+
+        /*$statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY :sort');*/
+
+
+        //var_dump($sort);
+        //$statement->bindParam(':sort', $sort);
+        $statement->execute();
+        while ($report = $statement->fetch(pdo::FETCH_ASSOC)) {
+            $reportDate = new DateTime($report['date']);
+            $report['date'] = date_format($reportDate, "d/m/Y");
+            $reportList[] = $report;
+        }
+        return $reportList;
     }
 }
+/*
+function filterReport($date, $animalId, $statement)
+{
+    if ($date === null && $animalId === null) {
+        $statement;
+    }
+}*/
+/*
+//par defaut
+$statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY :sort');
+
+//un filtre actif
+$statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY :sort
+                                                                  WHERE date = :date');
+
+//deux filtre actif
+$statement = $this->connection->getConnection()->prepare('SELECT date, food_type as foodType, quantity, status_detail as statusDetail, animals.name as animalName
+                                                                  FROM veterinarian_report
+                                                                  LEFT JOIN animals ON veterinarian_report.animal_id = animals.id
+                                                                  ORDER BY :sort
+                                                                  WHERE :date = :date AND animals.id = :animal');
+*/
