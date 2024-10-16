a17b6cf06edeac44cae5e76760de234fdaac7965
Guillaume Theillier <guillaume.theillier@gmail.com>
2024-10-16 18:46:46 +0200

    update animal page information.

--- templates/animal.php
+++ templates/animal.php
@@ -5,12 +5,14 @@ ob_start();
 
 <main class="animal-container">
     <h1 class="page-title"><?php echo $animal['name'] ?></h1>
-    <img class="animal-img" src="data:image/jpg;base64,<?php echo $animal['image'] ?>" alt="">
 
-    <div class="animal-description">
-        <p class="animal-habitat">habitat : <?php echo $animal['habitat'] ?> </p>
-        <p class="animal-race">Race : <?php echo $animal['race'] ?> </p>
-        <p class="animal-status">Etat de l'animal : <?php echo $animal['status'] ?></p>
+    <div id="animal-flex">
+        <img class="animal-img" src="data:image/jpg;base64,<?php echo $animal['image'] ?>" alt="">
+        <div class="animal-description">
+            <p class="animal-habitat">habitat : <?php echo $animal['habitat'] ?> </p>
+            <p class="animal-race">Race : <?php echo $animal['race'] ?> </p>
+            <p class="animal-status">Etat de l'animal : <?php echo $animal['status'] ?></p>
+        </div>
     </div>
 </main>
 
